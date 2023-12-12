80afc3077948ae8effb1484ada69773137078de6
bybuss <kukuhaha729@gmail.com>
2023-12-12 12:04:34 +0300

    ㅤ

---
+++ ГДЗ/second_module/05 Контейнер map/КОД/HomeWork5/HomeWork5/HomeWork5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+	map<string, int> shopping_list;
+
+	shopping_list["Apple"] = 12;
+	shopping_list["Tea"] = 2;
+	shopping_list["Coffee"] = 1;
+	shopping_list["Cheese"] = 3;
+	shopping_list["Orange"] = 9;
+
+	cout << "first map: " << endl;
+	for (auto i : shopping_list) {
+		cout << i.first << ": " << i.second << endl;
+	}
+
+	shopping_list["Pen"] = 6;
+	shopping_list["Bob_Colbaskin"] = 123321;
+
+	cout << endl << "second map: " << endl;
+	for (const auto& i : shopping_list) {
+		cout << i.first << ": " << i.second << endl;
+	}
+}
