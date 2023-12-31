f5b51b8f4d79cd940cebefc75a380b47ec65c885
bybuss <kukuhaha729@gmail.com>
2023-12-31 14:58:24 +0300

    ㅤ

---
+++ ГДЗ/second_module/08 Работа с файлами/КОД/HomeWork8/HomeWork8/HomeWork8.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string> 
+#include <Windows.h>
+
+using namespace std;
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string name;
+	string path = "bob.txt";
+
+	cout << "Введите ваше имя: ";
+	getline(cin, name);
+	
+	ofstream fout(path);
+	if (!fout.is_open()) { 
+		cout << "Файл не был открыт! " << endl; 
+		return 1; 
+	}
+	else {
+		fout << "Привет, " << name << ", ты записан в файл!!" << endl;
+	} 
+	fout.close();
+
+	ifstream fin(path);
+	if (!fin.is_open()) { 
+		cout << "Файл не был открыт! " << endl; 
+		return 1;
+	}
+	else { 
+		string data;
+		while (getline(fin, data)) { 
+			cout << "Содержимое файла: " << data << " " << endl;
+		} 
+	} 
+	fin.close();
+}
